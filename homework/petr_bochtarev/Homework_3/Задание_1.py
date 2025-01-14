24c28505b45cfc8ccb3b627a2365b797e5f5f513
Bochtarev Petr <bochtarev@proitr.ru>
2025-01-15 01:30:38 +0300

    Homework_3 done

---
+++ homework/petr_bochtarev/Homework_3/Задание_1.py
@@ -0,0 +1,14 @@
+first_number = 10
+second_number = 5
+sum_of_numbers = first_number + second_number
+number_difference = first_number - second_number
+multiplying_numbers = first_number * second_number
+print('Сумма чисел равна:', sum_of_numbers)
+print('Разность чисел равна:', number_difference)
+print('Произведение чисел равно:', multiplying_numbers)
+print()
+
+# На всякий случай добавлю и второй вариант:
+print('Сумма чисел равна:', first_number + second_number)
+print('Разность чисел равна:', first_number - second_number)
+print('Произведение чисел равно:', first_number * second_number)
---
+++ homework/petr_bochtarev/Homework_3/Задание_2.py
@@ -0,0 +1,4 @@
+first_number = 100
+second_number = 20
+task = first_number - second_number / 1 + first_number * second_number
+print('Ответ:', task)
