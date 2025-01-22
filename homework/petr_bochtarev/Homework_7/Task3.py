ee22d34dec55dfb9c3df924790d2a64378c2859b
Bochtarev Petr <bochtarev@proitr.ru>
2025-01-22 21:34:53 +0300

    Побавлены по две строки перед и после функции. Перед return убран один лишний пробел

--- homework/petr_bochtarev/Homework_7/Task3.py
+++ homework/petr_bochtarev/Homework_7/Task3.py
@@ -2,8 +2,12 @@ one = 'результат операции: 42'
 two = 'результат операции: 514'
 three = 'результат работы программы: 9'
 four = 'результат: 2'
+
+
 def task(x):
-     return int(x.split()[-1]) + 10
+    return int(x.split()[-1]) + 10
+
+
 print(task(one))
 print(task(two))
 print(task(three))
